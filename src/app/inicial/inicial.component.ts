4197d2ed77e0c4b030c9c16463b4af25cd524004
EvokSix <70288620+EvokSix@users.noreply.github.com>
2023-02-06 20:33:35 -0300

    adicionado botao avancar e voltar para os personagens

--- src/app/inicial/inicial.component.ts
+++ src/app/inicial/inicial.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Pet } from '../shared/pet';
 
 @Component({
   selector: 'app-inicial',
@@ -6,15 +7,61 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./inicial.component.css'],
 })
 export class InicialComponent implements OnInit {
+  listaHerois!: Pet[];
+  posicaoSelecionada: number = 0;
+  classSelecionada!: string;
 
-  constructor(){}
+  constructor() {}
 
   ngOnInit(): void {
+    this.listaHerois = [
+      {
+        nome: 'Guerreiro',
+        poder: 0,
+        pontosExp: 0,
+        raca: 'Humano',
+        imagem: 'blackWarrior',
+      },
+      {
+        nome: 'Guerreiro 2',
+        poder: 0,
+        pontosExp: 0,
+        raca: 'Humano',
+        imagem: 'whiteWarrior',
+      },
+      {
+        nome: 'Guerreiro 3',
+        poder: 0,
+        pontosExp: 0,
+        raca: 'Humano',
+        imagem: 'archAngel',
+      },
+    ];
+
+    this.getPosicaoArray(0);
+  }
+
+  getPosicaoArray(posicao: number) {
+    this.classSelecionada = this.listaHerois[posicao].imagem;
+  }
+
+  avancar() {
+    this.posicaoSelecionada + 2 > this.listaHerois.length
+      ? (this.posicaoSelecionada = 0)
+      : this.posicaoSelecionada++;
+      this.getPosicaoArray(this.posicaoSelecionada);
+  }
+
+  voltar() {
+
+    this.posicaoSelecionada - 1 < 0
+      ? (this.posicaoSelecionada = this.listaHerois.length-1)
+      : this.posicaoSelecionada--;
+      this.getPosicaoArray(this.posicaoSelecionada);
 
   }
 
-  estudar(){
-    alert("entrou");
+  estudar() {
+    alert('entrou');
   }
-
 }
